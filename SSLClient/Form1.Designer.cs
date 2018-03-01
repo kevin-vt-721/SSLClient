bb5f052c78c7605fa4bc87a019388028047acb8f
kevin-vt-721 <red-skins89@hotmail.com>
2018-02-28 22:10:52 -0500

    Added Solution with intial connection test

---
+++ SSLClient/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace SSLClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testConnectionButton = new System.Windows.Forms.Button();
+            this.statusResult = new System.Windows.Forms.TextBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // testConnectionButton
+            // 
+            this.testConnectionButton.Location = new System.Drawing.Point(323, 186);
+            this.testConnectionButton.Name = "testConnectionButton";
+            this.testConnectionButton.Size = new System.Drawing.Size(687, 199);
+            this.testConnectionButton.TabIndex = 0;
+            this.testConnectionButton.Text = "Test Connection To Microservice";
+            this.testConnectionButton.UseVisualStyleBackColor = true;
+            this.testConnectionButton.Click += new System.EventHandler(this.testConnectionButton_Click);
+            // 
+            // statusResult
+            // 
+            this.statusResult.Location = new System.Drawing.Point(323, 436);
+            this.statusResult.Name = "statusResult";
+            this.statusResult.Size = new System.Drawing.Size(687, 38);
+            this.statusResult.TabIndex = 1;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(210, 436);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(104, 32);
+            this.statusLabel.TabIndex = 2;
+            this.statusLabel.Text = "Status:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1411, 749);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.statusResult);
+            this.Controls.Add(this.testConnectionButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button testConnectionButton;
+        private System.Windows.Forms.TextBox statusResult;
+        private System.Windows.Forms.Label statusLabel;
+    }
+}
+
---
+++ SSLClient/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSLClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void testConnectionButton_Click(object sender, EventArgs e)
+        {
+            Uri uri = new Uri("http://localhost:8050/api/v1/testConnection");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            request.Method = "GET";
+            try
+            {
+                WebResponse webResponse = request.GetResponse();
+                Stream webStream = webResponse.GetResponseStream();
+                StreamReader responseReader = new StreamReader(webStream);
+                string response = responseReader.ReadToEnd();
+                this.statusResult.Text = response;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("Exception Caught: " + ex.ToString());
+            }
+        }
+    }
+}
